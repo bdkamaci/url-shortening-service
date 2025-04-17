8d9cf092365c739f40d0a0cf862982269e42afb0
bdkamaci <burcudogakamaci@gmail.com>
2025-04-17 13:25:42 +0200

    chore: update main entry points for application and tests

--- src/main/java/com/bdkamaci/urlshorteningservice/UrlShorteningServiceApplication.java
+++ src/main/java/com/bdkamaci/urlshorteningservice/UrlShorteningServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrlShorteningServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlShorteningServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorteningServiceApplication.class, args);
+    }
 
 }
--- src/test/java/com/bdkamaci/urlshorteningservice/UrlShorteningServiceApplicationTests.java
+++ src/test/java/com/bdkamaci/urlshorteningservice/UrlShorteningServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.bdkamaci.urlshorteningservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
-@SpringBootTest
+import java.io.PrintWriter;
+
 class UrlShorteningServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(DiscoverySelectors.selectPackage("com.bdkamaci"))
+                .build();
 
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        LauncherFactory.create().execute(request, listener);
+
+        TestExecutionSummary summary = listener.getSummary();
+        summary.printTo(new PrintWriter(System.out));
+
+        if (summary.getTotalFailureCount() > 0) {
+            System.exit(1);
+        } else {
+            System.exit(0);
+        }
+    }
 }
